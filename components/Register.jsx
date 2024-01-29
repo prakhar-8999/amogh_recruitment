fcf62673018ef6663ed9ada78dc46c803c91d61b
prakhar porwal <prakharporwal12a@gmail.com>
2024-01-29 23:51:39 +0530

    navigate to register

--- components/Register.jsx
+++ components/Register.jsx
@@ -11,7 +11,7 @@ const Register = () => {
   };
   return (
     <>
-      <section className="text-gray-900 body-font bg-slate-100">
+      <section className="text-gray-900 body-font bg-slate-100" id="register">
         <div className="container px-5 sm:px24 py-6 mx-auto flex flex-wrap items-center">
           <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
             <h1 className="title-font font-medium text-3xl text-gray-900">
