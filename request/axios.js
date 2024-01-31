6e66f23f83e5d0113b92643cf567db46e8a4b716
prakhar porwal <prakharporwal12a@gmail.com>
2024-01-31 22:04:24 +0530

    base url update

--- request/axios.js
+++ request/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "https://c940-125-21-249-98.ngrok-free.app/api/user",
-  timeout: 1000,
-  headers: {"X-Custom-Header": "foobar"},
+  baseURL: "https://6dd72743d93f51555d0a24bea844d3e8.serveo.net/api/user",
 });
 
 export default client;
